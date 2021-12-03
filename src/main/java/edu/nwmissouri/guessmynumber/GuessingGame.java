33cc5344f6243cb2d8fff9733fc6ff84aece74a5
rehana7 <89412687+rehana7@users.noreply.github.com>
2021-12-03 11:40:39 -0600

    added a text

--- src/main/java/edu/nwmissouri/guessmynumber/GuessingGame.java
+++ src/main/java/edu/nwmissouri/guessmynumber/GuessingGame.java
@@ -47,7 +47,7 @@ public class GuessingGame {
         if (userAnswer <= 0 || userAnswer > 100) {
             return "Your guess is invalid";
         } else if (userAnswer == computerNumber) {
-            return "Correct!\nTotal Guesses: " + count;
+            return "Hurray !! Correct!\nTotal Guesses: " + count;
         } else if (userAnswer > computerNumber) {
             return "Your guess is too high, try again.\nTry Number: " + count;
         } else if (userAnswer < computerNumber) {
